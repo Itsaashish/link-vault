225805eb9a7e81283b4a7327b1502e07f8a3208f
AashishKumar Yadav <yadavaashish2709@gmail.com>
2025-03-09 19:10:11 +0530

    removed unused code

--- lib/db.ts
+++ lib/db.ts
@@ -1,23 +1,3 @@
-// import { MongoClient } from "mongodb";
-
-// const MONGODB_URI =
-//   process.env.MONGODB_URI || "mongodb://localhost:27017/link-saver";
-
-// let client: MongoClient;
-// let clientPromise: Promise<MongoClient>;
-
-// if (!global._mongoClientPromise) {
-//   client = new MongoClient(MONGODB_URI);
-//   global._mongoClientPromise = client.connect();
-// }
-
-// clientPromise = global._mongoClientPromise;
-
-// export async function connectToDatabase() {
-//   const client = await clientPromise;
-//   return client.db();
-// }
-
 import { MongoClient, ObjectId } from "mongodb";
 
 const MONGODB_URI =
