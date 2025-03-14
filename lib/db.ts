e905950ced507b93bb4450285cb582b377495ee0
AashishKumar Yadav <yadavaashish2709@gmail.com>
2025-03-14 13:04:08 +0530

    Fix ESLint errors in db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,28 @@
+// import { MongoClient } from "mongodb";
+
+// const MONGODB_URI =
+//   process.env.MONGODB_URI || "mongodb://localhost:27017/link-saver";
+
+// // Add this type declaration for the global object
+// declare global {
+//   var _mongoClientPromise: Promise<MongoClient>;
+// }
+
+// let client: MongoClient;
+// let clientPromise: Promise<MongoClient>;
+
+// if (!global._mongoClientPromise) {
+//   client = new MongoClient(MONGODB_URI);
+//   global._mongoClientPromise = client.connect();
+// }
+
+// clientPromise = global._mongoClientPromise;
+
+// export async function connectToDatabase() {
+//   const client = await clientPromise;
+//   return client.db();
+// }
+
 import { MongoClient } from "mongodb";
 
 const MONGODB_URI =
@@ -9,14 +34,13 @@ declare global {
 }
 
 let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-
-if (!global._mongoClientPromise) {
-  client = new MongoClient(MONGODB_URI);
-  global._mongoClientPromise = client.connect();
-}
-
-clientPromise = global._mongoClientPromise;
+const clientPromise: Promise<MongoClient> =
+  global._mongoClientPromise ||
+  (() => {
+    client = new MongoClient(MONGODB_URI);
+    global._mongoClientPromise = client.connect();
+    return global._mongoClientPromise;
+  })();
 
 export async function connectToDatabase() {
   const client = await clientPromise;
